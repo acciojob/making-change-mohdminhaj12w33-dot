380c353e37bfbb16537efebc8b036dcd473dcb28
mohdminhaj12w33-dot <mohdminhaj12w33@gmail.com>
2025-12-03 23:00:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const makeChange = (c) => {
-  // your name here
+const makeChange = (amount) => {
+    amount = Number(amount); // convert prompt input to number
+    const change = {};
+
+    change.q = Math.floor(amount / 25);
+    amount %= 25;
+
+    change.d = Math.floor(amount / 10);
+    amount %= 10;
+
+    change.n = Math.floor(amount / 5);
+    amount %= 5;
+
+    change.p = amount;
+
+    return change;
 };
 
-// Do not the change the code below
+// Do not change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
